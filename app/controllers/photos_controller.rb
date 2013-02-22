b4fcdb989a11256851b4b884df4405d2f1a22d99
cxfcxf <siegfried.chen@gmail.com>
2013-02-22 00:03:44 -0800

    4th

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,8 +4,26 @@ class PhotosController < ApplicationController
 	end
 	
 	def create
-		tmp_file = params[:photos][:imgdir]
+#		binding.pry
+		tmp_file = params[:photo][:imgdir]
 		t = Time.now.to_i
-		FileUtils.mv tmp_file, Rails.root.join("imgfile", t.to_s)
+		suffix = tmp_file.original_filename.split('.').last
+		filesavepath = Rails.root.join("imgfile", t.to_s + '.' + suffix)
+		Photo.create(:user_id => '1', :imgdir => filesavepath.to_s)
+		
+		FileUtils.mv tmp_file.path, filesavepath
+		
+		if $? == 0
+			redirect_to :action => 'index'
+		else
+			redirect_to :action => 'index'
+		end
 	end
+	
+	def upload_successed!
+	end
+	
+	def upload_failed!
+	end
+	
 end
