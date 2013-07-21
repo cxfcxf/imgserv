0181921f6cb90e031a474f47a7da602fc0e60711
cxfcxf <siegfried.chen@gmail.com>
2013-07-20 18:44:34 -0700

    0.91

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@images = Photo.find(:all, :limit => "255")
+		@images = Photo.find(:all, :limit => "255", :order => "RAND()")
 		if user_signed_in?
 			redirect_to :controller => "photos", :action => "new"
 		end
